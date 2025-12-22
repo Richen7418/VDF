6efeda728059c9581ba8cff6ef493dac28799a53
Richen7418 <61117748+Richen7418@users.noreply.github.com>
2025-12-22 20:45:16 +0800

    Refactor video path and data path in run_demo.sh

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -1,5 +1,6 @@
 data_path=data
+video_name=May_45s.mp4
 # data process
-CUDA_VISIBLE_DEVICES=0 python utils.py --video_path data/May_45s.mp4 --save_path data/ori_imgs
+CUDA_VISIBLE_DEVICES=0 python utils.py --video_path ${data_path}/${video_name} --save_path ${data_path}/ori_imgs
 # defense
-CUDA_VISIBLE_DEVICES=0 python fft_ifftmask.py --data_path data
+CUDA_VISIBLE_DEVICES=0 python fft_ifftmask.py --data_path $data_path
