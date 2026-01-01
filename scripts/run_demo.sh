e0b26b29a93f348bd61274a4e2745a523a67cf7d
Richen7418 <61117748+Richen7418@users.noreply.github.com>
2026-01-01 16:51:49 +0800

    Modify run_demo.sh for preprocessing and reconstruction
    Updated run_demo.sh to include audio processing and video compositing.

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -1,6 +1,8 @@
 data_path=data
 video_name=May_45s.mp4
 # data process
-CUDA_VISIBLE_DEVICES=0 python utils.py --video_path ${data_path}/${video_name} --save_path ${data_path}/ori_imgs
+CUDA_VISIBLE_DEVICES=0 python utils.py --mode Preprocess --video_path ${data_path}/${video_name} --audio_path ${data_path}/audio.wav --image_path ${data_path}/ori_imgs 
 # defense
 CUDA_VISIBLE_DEVICES=0 python fft_ifftmask.py --data_path $data_path
+# Video compositing
+CUDA_VISIBLE_DEVICES=0 python utils.py --mode Reconstruction --adv_images_path ${data_path}/ifftmask_fft_adv_imgs/full_face/epsilon_0.05/ --audio_path ${data_path}/audio.wav --output_video ${data_path}/adv_output.mp4  --key_word full_face
